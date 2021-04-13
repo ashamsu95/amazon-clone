1404169be5c95574422ec59c0799b611f735fc96
ashamsu <ashamsuu@gmail.com>
2021-04-14 01:23:28 +0530

    initial

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+       <h1>hello</h1>
+       {/*Headr*/}
+       {/*Home*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrq0t4jgHeBpsTsZZeeAPRk6EvD4xUmFk",
+    authDomain: "clone-a4a73.firebaseapp.com",
+    projectId: "clone-a4a73",
+    storageBucket: "clone-a4a73.appspot.com",
+    messagingSenderId: "994291778895",
+    appId: "1:994291778895:web:ea826e3cd75da3df03f13a",
+    measurementId: "G-36JTTHHT5Y"
+  };
