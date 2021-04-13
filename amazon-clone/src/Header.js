1680b74af6e7e07d12f44288f46dc198ff0c914a
ashamsu <ashamsuu@gmail.com>
2021-04-14 01:47:56 +0530

    Header made

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -4,7 +4,14 @@ import './header.css'
 function Header() {
     return (
         <div className='header'>
+            <img className='header__logo' src='https://www.amazon.in/ref=nav_logo'></img>
             
+            <div className="header__search">
+                <input className='header__searchInput' type="text">
+                {/* LOGO */}
+                </input>
+            </div>
+
         </div>
     )
 }
